2c3b6c487679bdbc1a7996e0826cad01253d3d9a
Vadim Ostafiychuk <vadym.ostafiychuk@riseserv.com>
2020-08-25 22:09:04 +0300

    Cookie

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 const app = express()
 const port = process.env.PORT || 3040
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cookieParser({
+  maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+  httpOnly: false, // The cookie only accessible by the web server
+}));
+
+app.get('/login', (req, res) => {
+
+  res.cookie('auth', 'fisojenwffjewakj2395235asdwqf', {maxAge: 1000 * 120 * 15,httpOnly: false})
+  res.status(200).json({msg: 'Test Server'})
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
